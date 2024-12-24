78baf0f9fd772f6431f4ac654ed84953417291db
Bhanu-Vangapally <vangapallybhanu18070@gmail.com>
2024-12-24 20:11:30 +0530

    5th Day

---
+++ Implement-stack-using-array.java
@@ -0,0 +1,20 @@
+class MyStack {
+    private int[] arr;
+    private int top;
+
+    public MyStack() {
+        arr = new int[1000];
+        top = -1;
+    }
+
+    public void push(int x) {
+        // Your Code
+        arr[++top]=x;
+    }
+
+    public int pop() {
+        // Your Code
+        if(top>=0) return arr[top--];
+        else return -1;
+    }
+}
