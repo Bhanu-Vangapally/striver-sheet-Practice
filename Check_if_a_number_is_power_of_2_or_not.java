5f3d7a27239a5f4487dfa17dbc61513a27fa99aa
Bhanu-Vangapally <vangapallybhanu18070@gmail.com>
2025-01-18 21:54:08 +0530

    heck if a number is power of 2 or not 29th Day

---
+++ Check_if_a_number_is_power_of_2_or_not.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        int c=0;
+        while(n>0){
+            if((n&1)==1) c++;
+            n>>=1;
+        }
+        return c==1;
+    }
+}
