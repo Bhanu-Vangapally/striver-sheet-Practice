9cc57871aaf92c97004bc1e6f095a83ad26ad830
Bhanu-Vangapally <vangapallybhanu18070@gmail.com>
2025-07-17 23:49:02 +0530

    Array to ArrayList

---
+++ Array_to_ArrayList.java
@@ -0,0 +1,8 @@
+class Solution {
+    public static ArrayList<Integer> fillArrayList(int arr[]) {
+        // Your code here
+        ArrayList<Integer> l=new ArrayList<>();
+        for(int i:arr) l.add(i);
+        return l;
+    }
+}
