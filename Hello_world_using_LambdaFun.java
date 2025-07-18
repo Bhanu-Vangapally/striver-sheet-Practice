13f6ebd4e768b60dc89c145ccebc97a92fdae808
Bhanu-Vangapally <vangapallybhanu18070@gmail.com>
2025-07-18 12:54:51 +0530

    sayHello using lambda expression

---
+++ Hello_world_using_LambdaFun.java
@@ -0,0 +1,10 @@
+class Solution {
+    public static Hello helperFunction() {
+        // Your code here
+        
+        Hello h=()-> System.out.println("Hello");
+        return h;
+        // Implement sayHello using lambda expression and return the object.
+        // Write this in the lambda expression: System.out.println("Hello")
+    }
+}
