b079296ac83e957bd40a6094d66ab60499a49d9a
Bhanu-Vangapally <vangapallybhanu18070@gmail.com>
2025-07-23 17:37:37 +0530

    Linked_list_traverse_using_foreach_loop.java

---
+++ Linked_list_traverse_using_foreach_loop.java
@@ -0,0 +1,7 @@
+// User function Template for Java
+class Solution {
+    public static void LinkedListTraversal(LinkedList<Integer> list) {
+        // Your code here
+        list.forEach(i->System.out.print(i+" "));
+    }
+}
