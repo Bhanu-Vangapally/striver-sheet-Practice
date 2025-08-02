256a9f9c58481831d637ca5a0518551d620151da
Bhanu-Vangapally <vangapallybhanu18070@gmail.com>
2025-08-02 21:16:42 +0530

    Queue_printing

---
+++ Queue_traversal.java
@@ -0,0 +1,8 @@
+// User function Template for Java
+class Solution {
+    public static void queueTraversal(Queue<Integer> q) {
+        // Your code here
+        while(!q.isEmpty())
+        System.out.print(q.poll()+" ");
+    }
+}
