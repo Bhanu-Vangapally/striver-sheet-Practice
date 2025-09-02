9cf24783dacbd14d54773f6344d1d4776758202a
Bhanu-Vangapally <vangapallybhanu18070@gmail.com>
2025-09-03 00:20:35 +0530

    LL fill using Collection class

---
+++ fill LL using Collection.java
@@ -0,0 +1,7 @@
+// User function Template for Java
+class Solution {
+    public static void fillLinkedList(LinkedList<Integer> ll, int k) {
+        // Your code here
+        Collections.fill(ll,k);
+    }
+}
