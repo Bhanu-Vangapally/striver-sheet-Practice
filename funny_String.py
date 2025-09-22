c7d4d3d2083b2246e291e32de2746c3bb6d042e2
Bhanu-Vangapally <vangapallybhanu18070@gmail.com>
2025-09-22 12:31:45 +0530

    Funny String Hacker Rank Qn by Python

---
+++ funny_String.py
@@ -0,0 +1,28 @@
+
+import math
+import os
+import random
+import re
+import sys
+
+def funnyString(s):
+    # Write your code here
+    r=s[::-1]
+    for i in range(1,len(r)):
+        if(abs(ord(s[i])-ord(s[i-1]))!=abs(ord(r[i])-ord(r[i-1]))):
+            return "Not Funny"
+    return "Funny"
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        s = input()
+
+        result = funnyString(s)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
